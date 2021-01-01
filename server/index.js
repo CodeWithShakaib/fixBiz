cf3dddc7ec6ccb7f7d616abfaed4f3f078bfb987
CodeWithShakaib <shakaibhassan16@gmail.com>
2021-01-01 14:42:30 +0500

    fix port issue

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require("./config/express")
-const port = 3000
+const port = process.env.PORT || 3000
 
 const http = require('http').createServer(app); // http server use for whole application
 
