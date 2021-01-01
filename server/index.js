7f6bab1e140a182fdaa2de8523a40fdfacfe0ccc
CodeWithShakaib <shakaibhassan16@gmail.com>
2021-01-01 12:58:17 +0500

    initial route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const port = 3000
 
 const http = require('http').createServer(app); // http server use for whole application
 
+app.get('/', function(req, res) {
+    res.send("Welcome to fiz-biz");
+});
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
